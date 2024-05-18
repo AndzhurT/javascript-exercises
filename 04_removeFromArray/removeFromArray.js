bde3c6bbfe0c60750a4ac9ce99d2bd2b6ed08a51
AndzhurT <andjur38@gmail.com>
2024-05-17 22:46:08 -0400

    Pass exercise 4
    Use splice to delete elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value1 = null, value2 = null, value3 = null, value4 = null) {
 
+    for(let i = array.length - 1; i >= 0; i--) {
+        
+        if (array[i] === value1 || array[i] === value2 || array[i] === value3 || array[i] === value4) {
+            array.splice(i, 1)
+        } 
+    }
+    return array;
 };
 
 // Do not edit below this line
