666d9810354dbc4872f9802b062a31e78aa5ac08
AndzhurT <andjur38@gmail.com>
2024-05-17 22:23:31 -0400

    Pass exercise 1 and 2
    Pass exercise 2 by creating a for loop and some basic conditional statements

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(text, numberToRepeat) {
+    let finalText = text;
 
+    if (numberToRepeat < 0) {
+        return 'ERROR';
+    }
+    else if (numberToRepeat == 0) {
+        return '';
+    }
+    else {
+        for (let i = 0; i < numberToRepeat - 1; i++) {
+            finalText += text;
+        };
+    }
+
+    return finalText
 };
 
 // Do not edit below this line
