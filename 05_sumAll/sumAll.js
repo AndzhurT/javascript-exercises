5685cbe8c79359b80f32ed6b8fc2882eb2a8ab87
AndzhurT <andjur38@gmail.com>
2024-05-18 17:40:50 -0400

    Finish exercise 5
    Use typeof and negative numbers check for error handling, everything else sums up

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    let sum = 0;
 
+    if (typeof(from) != "number" || typeof(to) != "number" || from < 0 || to < 0) {
+        return "ERROR";
+    } 
+    else {
+        if (from > to) {
+            [to, from] = [from, to];
+        }
+    
+        for (let i = from; i <= to; i++) {
+            sum += i;
+        }
+    }
+
+
+    return sum;
 };
 
 // Do not edit below this line
