c5442c00e1fe60291c9c1f62199481266644ecf7
AndzhurT <andjur38@gmail.com>
2024-05-27 21:42:56 -0400

    Finish exercise 10
    Simple fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    }
 
+    if (a == 0) {
+        return 0;
+    }
+    else if (a == 1) {
+        return 1;
+    }
+
+    return fibonacci(a - 2) + fibonacci(a - 1);
 };
 
 // Do not edit below this line
