af808b2ef49385bc034512125cd75d1e07148f2f
AndzhurT <andjur38@gmail.com>
2024-05-27 21:32:12 -0400

    Finish exercise 8 and 9
    Finish calculator.js and palindromes.js using reduce and regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase().split(" ").join("");
+    let left = 0;
+    let right = word.length - 1;
+
+    while (left < right) {
+        if (word[left] != word[right]) {
+            return false;
+        }
+        
+        left += 1;
+        right -= 1;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
